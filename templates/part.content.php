acd9addae92ec4d7868a2c639d6a7afafc74b2d8
Fallon Turner <fcturner@users.noreply.github.com>
2016-02-20 21:44:45 +0100

    Update part.content.php

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,86 +1,156 @@
 <?php
 $days_orange = \OCP\Config::getAppValue('passwords', 'days_orange', '150');
 $days_red = \OCP\Config::getAppValue('passwords', 'days_red', '365');
+$version = \OCP\Config::getAppValue('passwords', 'installed_version', '');
 ?>
-
-<div id="cleartrashbin">
-	<input type="button" id="delete_trashbin" value="<?php p($l->t("Delete all items in trash bin")); ?>">
+<div class="icon-loading" id="loading">
+	<p id="loading_text"><?php p($l->t("Decrypting passwords")); ?>...</p>
 </div>
 
-<div id="PasswordsTable">
+<div id="section_update">
+	<div id="update_box">
+		<h2><?php p($l->t("Update required")); ?></h2>
+		<p id="update_p_start"><?php p($l->t("%s has been updated to version %s, which requires an update of your passwords table. Do not close this window during the update.", array($theme->getName() . ' ' . $l->t("Passwords"), $version))); ?></p>
+		<input type="button" id="update_start_btn" value="<?php p($l->t("Update")); ?>">
+		<div id="update_progress">
+			<input id="update_progress_active"/>
+			<input id="update_progress_total"/>
+			<p id="update_progress_text">0 / 0</p>
+			<div id="update_progress_bar">
+				<div id="update_progress_done"></div>
+			</div>
+		</div>
+		<div id="update_done">
+			<p id="update_p_done"><b><?php p($l->t("Update done. This page must now reload.")); ?></b></p>
+			<input type="button" id="update_done_btn" value="<?php p($l->t("Reload")); ?>">
+		</div>
+	</div>
+	<div id="update_overlay"></div>
+</div>
+<div id="section_table">
+	<div id="cleartrashbin">
+		<input type="button" id="delete_trashbin" value="<?php p($l->t("Delete all items in trash bin")); ?>">
+	</div>
 
-	<table id="PasswordsTableContent" class="sortable">
-		<tr>
-			<th id="column_website"><?php p($l->t("Website or company")); ?></th>
-			<th id="FieldLengthCheck" class="sorttable_alpha"><?php p($l->t("Login name")); ?></th>
-			<th id="FieldLengthCheck" class="sorttable_alpha"><?php p($l->t("Password")); ?></th>
-			<th id="hide3"><?php p($l->t("Strength")); ?></th>
-			<th class="hide_attributes sorttable_numeric" id="hide1"><?php p($l->t("Length")); ?></th>
-			<th class="hide_attributes" id="hide1">a-z</th>
-			<th class="hide_attributes" id="hide1">A-Z</th>
-			<th class="hide_attributes" id="hide1">0-9</th>
-			<th class="hide_attributes" id="hide1">!@#</th>
-			<th id="hide2"><?php p($l->t("Last changed")); ?></th>
-			<th id="hide_always">ID</th>
-			<th id="hide_always">User-ID</th>
-			<th id="hide_always">Address</th>
-			<th id="column_notes"></th>
-			<th id="column_delete"></th>
-			<th id="hide_always">Deleted</th>
-			<th id="column_sidebar"></th>
-			<!-- <th id="column_share"></th> -->
-		</tr>
+	<div id="commands_popup">
+		<input type="button" id="btn_edit" value="<?php p(strtolower($l->t("Edit"))); ?>">
+		<input type="button" id="btn_copy" value="<?php p(strtolower($l->t("Copy"))); ?>">
+		<!-- <input type="button" id="btn_share" value="<?php p(strtolower($l->t("Share"))); ?>"> -->
+		<input type="text" id="cmd_id">
+		<input type="text" id="cmd_type">
+		<input type="text" id="cmd_value">
+		<input type="text" id="cmd_website">
+		<input type="text" id="cmd_address">
+		<input type="text" id="cmd_loginname">
+		<input type="text" id="cmd_pass">
+		<input type="text" id="cmd_notes">
+		<input type="text" id="cmd_category">
+	</div>
+	<div id="zeroclipboard_copied">
+		<p id="copied_text"><?php p($l->t("Value copied to clipboard")); ?></p>
+	</div>
 
-	</table>
+	<div id="PasswordsTable">
 
-	<br>
-	<p align="center"><?php print_unescaped($l->t("Click on a <b>column head</b> to sort the column.")); ?></p>
-	<p align="center"><?php print_unescaped($l->t("Click on a <b>user name</b> or a <b>password</b> to be able to copy it to the clipboard.")); ?></p>
-	<p align="center"><?php print_unescaped($l->t("Click on a <b>website</b> to open it in a new tab.")); ?></p>
-	<br>
-	<p align="center"><?php print_unescaped($l->t("The <b>password date</b> becomes <orange>orange after %s days</orange> and <red>red after %s days</red>.", array($days_orange, $days_red))); ?></p>
-	<p align="center"><?php print_unescaped($l->t("The <b>strength value</b> is interpreted as") . " <red>" . strtolower($l->t("Weak")) .  "</red> (0-7), <orange>" . strtolower($l->t("Moderate")) . "</orange> (8-14) " . $l->t("or") . " <green>" . strtolower($l->t("Strong")) . "</green> (>= 15)."); ?></p>
-	<p align="center"><?php print_unescaped($l->t("The <b>password colour</b> is determined by the password strength and date (whichever comes first in weakness).")); ?></p>
-	<br>
+		<table id="PasswordsTableTestOld">
+			<!-- backwards compatibility for version prior to v17, will be used for update to new database form -->
+		</table>
 
+		<table id="PasswordsTableContent" class="sortable">
+			<tr>
+				<th id="column_website" class="sorttable_alpha"><?php p($l->t("Website or company")); ?></th>
+				<th id="column_username" class="sorttable_alpha"><?php p($l->t("Login name")); ?></th>
+				<th id="column_password" class="sorttable_alpha"><?php p($l->t("Password")); ?></th>
+				<th id="column_strength"><?php p($l->t("Strength")); ?></th>
+				<th id="column_datechanged"><?php p($l->t("Last changed")); ?></th>
+				<th id="column_category"><?php p($l->t("Category")); ?></th>
+				<th id="column_notes"></th>
+				<th id="column_info"></th>
+				<th id="column_delete"></th>
+			</tr>
+
+		</table>
+
+		<br>
+		<p align="center"><?php print_unescaped($l->t("Click on a <b>column head</b> to sort the column.")); ?></p>
+		<p align="center"><?php print_unescaped($l->t("Click on a <b>user name</b> or a <b>password</b> to be able to copy it to the clipboard.")); ?></p>
+		<p align="center"><?php print_unescaped($l->t("Click on a <b>website</b> to open it in a new tab.")); ?></p>
+		<br>
+		<p align="center"><?php print_unescaped($l->t("The <b>password date</b> becomes <orange>orange after %s days</orange> and <red>red after %s days</red>.", array($days_orange, $days_red))); ?></p>
+		<p align="center"><?php print_unescaped($l->t("The <b>strength value</b> is interpreted as") . " <red>" . strtolower($l->t("Weak")) .  "</red> (0-7), <orange>" . strtolower($l->t("Moderate")) . "</orange> (8-14) " . $l->t("or") . " <green>" . strtolower($l->t("Strong")) . "</green> (>= 15)."); ?></p>
+		<br>
+
+	</div>
+
+	<script id="template-passwords-serialize" type="text/x-handlebars-template">
+		{{#each passwords}}
+			"website" : "{{ website }}", "pass" : "{{ pass }}", {{ properties }}, "deleted" : "{{ deleted }}", "id" : "{{ id }}", "user_id" : "{{ user_id }}"<br>
+		{{/each}}
+	</script>
+
+	<script id="template-passwords-old" type="text/x-handlebars-template">
+
+		{{#each passwords}}
+			<tr>
+				<td>{{ website }}</td>
+				<td>{{ loginname }}</td>
+				<td>{{ pass }}</td>
+				<td>{{ properties }}</td>
+				<td>{{ creation_date }}</td>
+				<td>{{ id }}</td>
+				<td>{{ user_id }}</td>
+				<td>{{ address }}</td>
+				<td>{{ notes }}</td>
+				<td>{{ deleted }}</td>
+			</tr>
+		{{/each}}
+		
+	</script>
+
+	<div id="emptycontent">
+		<div class="icon-passwords"></div>
+		<h2><?php p($l->t("No passwords yet")); ?></h2>
+		<p><?php p($l->t("Create some new passwords!")); ?></p> 
+	</div>
+	<div id="emptytrashbin">
+		<div class="icon-delete"></div>
+		<h2><?php p($l->t("Empty trash bin")); ?></h2>
+		<p><?php p($l->t("Deleted passwords will be shown here!")); ?></p> 
+	</div>
 </div>
 
-<script id="content-tpl" type="text/x-handlebars-template">
+<div id="section_categories">
+	<input type="button" id="back_to_passwords" value="<< <?php p($l->t("Back to passwords")); ?>">
+	<br>
+	<h2><?php p($l->t("Create a new category")); ?></h2>
+	<input id="cat_name" type="text" value="" placeholder="<?php p($l->t("Name of category")); ?>...">
+	<input id="colorpicker" type="color" name="color">
+	<input id="cat_colour" type="text" value="#eeeeee">
+	<input type="button" id="cat_add" value="<?php p($l->t("Add category")); ?>">
+	<h2><?php p($l->t("List of categories")); ?></h2>
+	<div id="CategoriesTable">
+		<table id="CategoriesTableContent"></table>
+	</div>
 
-	{{#each passwords}}
-		<tr>
-			<td><div id="FieldLengthCheck">{{ website }}</div></td>
-			<td><div id="FieldLengthCheck">{{ loginname }}</div></td>
-			<td><div id="FieldLengthCheck">{{ pass }}</div></td>
-			<td id="hide3">(strength)</td>
-			<td class="hide_attributes" id="hide1">(length)</td>
-			<td class="hide_attributes" id="hide1">(a-z)</td>
-			<td class="hide_attributes" id="hide1">(A-Z)</td>
-			<td class="hide_attributes" id="hide1">(0-9)</td>
-			<td class="hide_attributes" id="hide1">(!@#)</td>
-			<td class="creation_date" id="hide2">{{ creation_date }}</td>
-			<td class="hide_always">{{ id }}</td>
-			<td class="hide_always">{{ user_id }}</td>
-			<td class="hide_always">{{ address }}</td>
-			<td class="icon-notes"><span>{{ notes }}</span></td>
-			<td class="icon-delete"></td>
-			<td class="hide_always">{{ deleted }}</td>
-			<td class="icon-info"></td>
-			<!--<td class="icon-share"></td>-->
-		</tr>
-	{{/each}}
-	
-</script>
+	<script id="template-categories" type="text/x-handlebars-template">
 
-<div id="emptycontent">
-	<div class="icon-passwords"></div>
-	<h2><?php p($l->t("No passwords yet")); ?></h2>
-	<p><?php p($l->t("Create some new passwords!")); ?></p> 
-</div>
-<div id="emptytrashbin">
-	<div class="icon-delete"></div>
-	<h2><?php p($l->t("Empty trash bin")); ?></h2>
-	<p><?php p($l->t("Deleted passwords will be shown here!")); ?></p> 
+		{{#each categories}}
+			<tr>
+				<td></td>
+				<td class="hide_always catTable_id">{{ id }}</td>
+				<td class="hide_always catTable_userid">{{ user_id }}</td>
+				<td class="hide_always catTable_name">{{ category_name }}</td>
+				<td class="hide_always catTable_bg">{{ category_colour }}</td>
+				<td class="icon-delete"></td>
+			</tr>
+		{{/each}}
+		
+	</script>
+
+	<div id="emptycategories">
+		<p><?php p($l->t("No categories yet")); ?>.</p> 
+	</div>
+
 </div>
 
 <div id="idleTimer">
