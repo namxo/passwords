a35696ecea120993229e3f720dacd0cdace1ed29
IzK666 <izk666@yandex.com>
2016-03-09 13:11:34 +0100

    Update categorymapper.php

--- db/categorymapper.php
+++ db/categorymapper.php
@@ -16,7 +16,7 @@ class CategoryMapper extends Mapper {
 	}
 
 	public function findAll($userId) {
-		$dbtype = \OC_Config::getValue('dbtype', '');
+		$dbtype = \OC::$server->getConfig()->getSystemValue('dbtype', '');
 		if ($dbtype == 'mysql') {
 			$sql = 'SELECT * FROM *PREFIX*passwords_categories WHERE user_id = ? ORDER BY LOWER(category_name) COLLATE utf8_general_ci ASC';
 		} else if ($dbtype == 'sqlite') {
