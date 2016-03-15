2c4538ee3bf66fdfa76ae58bbeb0e9b667f05b17
Fallon Turner <fcturner@users.noreply.github.com>
2016-03-15 22:52:12 +0100

    Update passwordmapper.php

--- db/passwordmapper.php
+++ db/passwordmapper.php
@@ -28,9 +28,14 @@ class PasswordMapper extends Mapper {
 	}
 
 	public function shareUsers($userId) {
-		$allowed = \OC::$server->getConfig()->getAppValue('core', 'shareapi_enabled', true);
+		$allowed = \OC::$server->getConfig()->getAppValue('core', 'shareapi_enabled', 'yes') == 'yes';
 		if ($allowed) {
-			$sql = 'SELECT DISTINCT displaynames.uid, displaynames.displayname FROM *PREFIX*group_user AS users LEFT JOIN (SELECT  uid, IF(displayname IS NULL, uid, displayname) AS displayname FROM *PREFIX*users) AS displaynames ON users.uid = displaynames.uid WHERE gid IN (SELECT DISTINCT gid FROM *PREFIX*group_user WHERE uid = ?';
+			$only_share_with_own_group = \OC::$server->getConfig()->getAppValue('core', 'shareapi_only_share_with_group_members', 'yes') == 'yes';
+			if ($only_share_with_own_group) {
+				$sql = 'SELECT DISTINCT displaynames.uid, displaynames.displayname FROM *PREFIX*group_user AS users LEFT JOIN (SELECT  uid, IF(displayname IS NULL, uid, displayname) AS displayname FROM *PREFIX*users) AS displaynames ON users.uid = displaynames.uid WHERE gid IN (SELECT DISTINCT gid FROM *PREFIX*group_user WHERE uid = ?';
+			} else {
+				$sql = 'SELECT uid, IF(displayname IS NULL, uid, displayname) AS displayname FROM *PREFIX*users';
+			}
 			return $this->findEntities($sql, [$userId]);
 		} else {
 			return false;
