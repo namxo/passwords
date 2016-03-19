e96a219182cb6dde232ab518f0c30f69f1946a34
Fallon Turner <fcturner@users.noreply.github.com>
2016-03-19 07:33:23 +0100

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -13,12 +13,12 @@ $l = \OCP\Util::getL10N('passwords');
 $l10n = $container->query('OCP\IL10N');
 
 $navigationEntry = function () use ($container) {
-return [
-'id' => 'passwords',
-'order' => 9999,
-'href' => $container->getServer()->getURLGenerator()->linkToRoute('passwords.page.index'),
-'icon' => $container->getServer()->getURLGenerator()->imagePath('passwords', 'app.svg'),
-'name' => $container->query('OCP\IL10N')->t('Passwords'),
-];
+	return [
+		'id' => 'passwords',
+		'order' => 9999,
+		'href' => $container->getServer()->getURLGenerator()->linkToRoute('passwords.page.index'),
+		'icon' => $container->getServer()->getURLGenerator()->imagePath('passwords', 'app.svg'),
+		'name' => $container->query('OCP\IL10N')->t('Passwords'),
+	];
 };
 $container->getServer()->getNavigationManager()->add($navigationEntry);
