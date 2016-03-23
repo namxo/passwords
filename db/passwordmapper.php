405eb6f5e80c398ed5e8f52d67f8ff34c4a3ecbf
Fallon Turner <fcturner@users.noreply.github.com>
2016-03-23 11:39:23 +0100

    Update passwordmapper.php

--- db/passwordmapper.php
+++ db/passwordmapper.php
@@ -1,8 +1,8 @@
 <?php
-namespace OCA\Passwords\Db;
+namespace \OCA\Passwords\Db;
 
-use OCP\IDb;
-use OCP\AppFramework\Db\Mapper;
+use \OCP\IDb;
+use \OCP\AppFramework\Db\Mapper;
 
 class PasswordMapper extends Mapper {
 
