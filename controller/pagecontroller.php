349f0705e61e1a94d6b5326a1f9984fdabb9085c
Fallon Turner <fcturner@users.noreply.github.com>
2016-03-23 15:00:52 +0100

    Update pagecontroller.php

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -1,23 +1,23 @@
 <?php
- namespace \OCA\Passwords\Controller;
+namespace \OCA\Passwords\Controller;
 
- use \OCP\IRequest;
- use \OCP\AppFramework\Http\TemplateResponse;
- use \OCP\AppFramework\Controller;
- use \OCP\AppFramework\Http\ContentSecurityPolicy;
+use \OCP\IRequest;
+use \OCP\AppFramework\Http\TemplateResponse;
+use \OCP\AppFramework\Controller;
+use \OCP\AppFramework\Http\ContentSecurityPolicy;
 
- class PageController extends Controller {
+class PageController extends Controller {
 
-	 public function __construct($AppName, IRequest $request) {
-		 parent::__construct($AppName, $request);
-	 }
+	public function __construct($AppName, IRequest $request) {
+		parent::__construct($AppName, $request);
+	}
 
-	 /**
-	  * @NoAdminRequired
-	  * @NoCSRFRequired
-	  */
-	 public function index() {
-		 if (substr(\OC_Util::getHumanVersion(), 0, 3) != '8.0') {
+	/**
+	* @NoAdminRequired
+	* @NoCSRFRequired
+	*/
+	public function index() {
+		if (version_compare(\OC_Util::getHumanVersion(), '8.1', '>=')) {
 			// OC >= 8.1 (OC9 too)
 			$response = new TemplateResponse('passwords', 'main');
 			$csp = new ContentSecurityPolicy();
@@ -26,10 +26,10 @@
 			$csp->addAllowedObjectDomain('\'self\''); // for clipboard function
 			$response->setContentSecurityPolicy($csp);
 			return $response;
-		 } else {
+		} else {
 			// OC =< 8.0.4
 			return new TemplateResponse('passwords', 'main');
-		 }
-	 }
+		}
+	}
 
- }
+}
