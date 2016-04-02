406348a3316f8a04cb3b5123a65234bfcc48f829
Fallon Turner <fcturner@users.noreply.github.com>
2016-04-02 22:54:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1068,7 +1068,7 @@
 						$('#new_category select').attr('style', '');
 						$('#generate_strength').text('');
 						$('#generate_passwordtools').fadeOut(250);
-						$('#gen_length').val('25');
+						$('#gen_length').val('30');
 
 						var passwords = new Passwords(OC.generateUrl('/apps/passwords/passwords'));
 						var view = new View(passwords);
