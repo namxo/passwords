9121f77ede3b72e89882cba3e5e7994462582e4b
Fallon Turner <fcturner@users.noreply.github.com>
2016-09-06 12:10:25 +0200

    Fix for Uninitialized string offset
    Error:
    Uninitialized string offset: 0 at /var/www/owncloud_prod/apps/passwords/service/passwordservice.php#360

--- service/passwordservice.php
+++ service/passwordservice.php
@@ -357,7 +357,11 @@ class Calculations {
 	public static function uniord($c) {
 		// http://stackoverflow.com/a/10333324
 		// used to replace JS's 'charCodeAt' function
-		$h = ord($c{0});
+		try {
+			$h = ord($c{0});
+		} catch (Exception $e) {
+			return false;
+		}	
 		if ($h <= 0x7F) {
 			return $h;
 		} else if ($h < 0xC2) {
